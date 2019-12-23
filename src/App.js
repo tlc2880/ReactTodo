e7b8cd53e67d9ea5762b20cc07f2305c3c397864
tlc2880 <tommylcao@gmail.com>
2019-12-23 15:54:45 -0800

    Improve functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 //     Name: Tommy Cao
 //     Date: 12/19/19
-//     Description: Todo CRUD React Application with Bootstrap
+//     Description: Todo React Application with Bootstrap
 
 import React, { Component } from 'react';
 import Projects from "./Components/Projects.js";
-
-// Scripts
 import 'popper.js/dist/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-// Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
